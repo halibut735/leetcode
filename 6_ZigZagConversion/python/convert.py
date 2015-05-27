a27e438882e802b7375a739e5ac2fa955ae8293a
halibut735 <827603830@qq.com>
2015-05-27 23:58:49 +0800

    half way on solution of problem 6th

---
+++ 6_ZigZagConversion/python/convert.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param {string} s
+    # @param {integer} numRows
+    # @return {string}
+    def convert(self, s, numRows):
+
+
+
+def main():
+    a = Solution
+    print a.convert('PAYPALISHIRING', 3)
+
+if __name__ == '__main__':
+    main()
