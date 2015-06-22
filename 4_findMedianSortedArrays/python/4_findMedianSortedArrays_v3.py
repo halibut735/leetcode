47dc20bb88d9429af386b9556d6f797a7b5669cc
halibut735 <827603830@qq.com>
2015-06-22 23:01:52 +0800

    accepted finally!!

--- 4_findMedianSortedArrays/python/4_findMedianSortedArrays_v3.py
+++ 4_findMedianSortedArrays/python/4_findMedianSortedArrays_v3.py
@@ -3,7 +3,27 @@ class Solution:
     # @param {integer[]} nums2
     # @return {float}
     def findMedianSortedArrays(self, nums1, nums2):
+        total = len(nums1) + len(nums2)
+        if total % 2:
+            return self.findKth(nums1, nums2, total/2 +1)
+        else:
+            return (self.findKth(nums1, nums2, total/2) + self.findKth(nums1, nums2, total/2 +1))/2.0
 
+    def findKth(self, nums1, nums2, k):
+        if len(nums1) > len(nums2):
+            return self.findKth(nums2, nums1, k)
+        if len(nums1) == 0:
+            return nums2[k-1]
+        if k == 1:
+            return min(nums1[0], nums2[0])
+        partionOfnum1 = min(len(nums1), k/2)
+        partionOfnum2 = k - partionOfnum1
+        if nums1[partionOfnum1 -1]  == nums2[partionOfnum2 -1]:
+            return nums1[partionOfnum1 -1]
+        elif nums1[partionOfnum1 -1] < nums2[partionOfnum2 -1]:
+            return self.findKth(nums1[partionOfnum1:], nums2, k -partionOfnum1)
+        else:
+            return self.findKth(nums1, nums2[partionOfnum2:], k -partionOfnum2)
 
 
 def main():
