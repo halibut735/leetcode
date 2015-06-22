a8432b7c6aa84064b8aadd08bc0e92ef39b0ae97
halibut735 <827603830@qq.com>
2015-06-22 14:29:00 +0800

    正确解法稍后奉上。。。

---
+++ 4_findMedianSortedArrays/python/4_findMedianSortedArrays_v3.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param {integer[]} nums1
+    # @param {integer[]} nums2
+    # @return {float}
+    def findMedianSortedArrays(self, nums1, nums2):
+
+
+
+def main():
+    nums1 = [3, 3, 6]
+    nums2 = [2, 4, 6]
+    a = Solution()
+    print a.findMedianSortedArrays(nums1, nums2)
+
+if __name__ == '__main__':
+    main()
