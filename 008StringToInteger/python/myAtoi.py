f4020bf70d2b5bc55656320dcbe0f90f7a8243c4
halibut735 <827603830@qq.com>
2015-06-23 12:50:32 +0800

    终于AC了。。天了噜，条件真多

--- 008StringToInteger/python/myAtoi.py
+++ 008StringToInteger/python/myAtoi.py
@@ -7,24 +7,32 @@ class Solution:
         MinInteger = -2147483648
         ret = 0
         if str == None or str == '':
-            return
+            return 0
+        while str[0] == ' ':
+            str = str[1:]
         if str[0] == '-' :
-            FlagOfNegative = True
+            FlagOfNegative = not FlagOfNegative
             str = str[1:]
-        if str[0] == '+':
+        elif str[0] == '+':
             str = str[1:]
+        if str[0] > '9' or str[0] < '0':
+            return 0
         for each in str:
-            if each <'9' and each > '0':
+            if each <='9' and each >= '0':
                 ret = ret *10 + int(each)
+            else:
+                break
         if FlagOfNegative:
             ret = -1 * ret
-        if ret > MaxInteger or ret < MinInteger:
-            return 0
+        if ret > MaxInteger:
+            return MaxInteger
+        elif ret < MinInteger:
+            return MinInteger
         return ret
 
 def main():
     test = Solution()
-    print test.myAtoi('-143432')
+    print test.myAtoi('   -1434aa32000')
 
 if __name__ == '__main__':
     main()
