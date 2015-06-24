61d1b76e53d4edb4f9b866317f85db778002c8d6
halibut735 <827603830@qq.com>
2015-06-24 23:51:37 +0800

    debug for endless loop...really annoying~

--- 010_Regular_Expression_Matching/python/isMatch_v2.py
+++ 010_Regular_Expression_Matching/python/isMatch_v2.py
@@ -4,14 +4,18 @@ Url: https://leetcode.com/problems/regular-expression-matching/
 Author: halibut735
 Data: 15.6.24
 '''
-
+import pdb
 class Solution:
     # @param {string} s
     # @param {string} p
     # @return {boolean}
+    def __init__(self):
+        self.count = 0
     def isMatch(self, s, p):
+        self.whileloop = 0
+        self.count += 1
         print 's=', s, 'p=', p
-
+        pdb.set_trace()
         '''
         boundary conditions...
         '''
@@ -28,16 +32,20 @@ class Solution:
 
         #when p[1] != * or len(p) == 1
         if len(p) == 1 or p[1] != '*':
+            print 'if count' , self.count
             if p[0] == '.' or (len(s) != 0 and p[0] == s[0]):
                 return self.isMatch(s[1:], p[1:])
             else:
                 return False
         else:
+            print 'else count', self.count
             return self.starMatch(s, p)
 
     def starMatch(self, s, p):
         #when p[1] == '*', we need to discuss whether p[0] matches or not
         while p[0] == '.' or (len(s) != 0 and p[0] == s[0]):
+            self.whileloop += 1
+            print 'whileloop: ', self.whileloop
             if self.isMatch(s[1:], p):
                 return True
         return self.isMatch(s, p[2:])
@@ -45,7 +53,7 @@ class Solution:
 
 def main():
     test = Solution()
-    print test.isMatch('aa', "a*a")
+    print test.isMatch('a', "a*a")
 
 if __name__ == '__main__':
     main()
