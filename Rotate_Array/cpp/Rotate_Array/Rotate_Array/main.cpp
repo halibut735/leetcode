87dd53dcd8df958086ef42927ff53670d01021b3
halibut735 <827603830@qq.com>
2015-09-10 14:13:31 +0800

    Initial Commit

---
+++ Rotate_Array/cpp/Rotate_Array/Rotate_Array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rotate_Array
+//
+//  Created by 中央政治局常委 on 15/9/10.
+//  Copyright (c) 2015年 中央政治局常委. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
