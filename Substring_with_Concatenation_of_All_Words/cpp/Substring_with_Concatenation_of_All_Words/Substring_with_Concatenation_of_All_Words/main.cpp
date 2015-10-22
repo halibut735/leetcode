36d780e896a6f4cdc0f3f7ef50b53d1ce262f6e2
halibut735 <827603830@qq.com>
2015-10-22 14:43:39 +0800

    hahaha

--- Substring_with_Concatenation_of_All_Words/cpp/Substring_with_Concatenation_of_All_Words/Substring_with_Concatenation_of_All_Words/main.cpp
+++ Substring_with_Concatenation_of_All_Words/cpp/Substring_with_Concatenation_of_All_Words/Substring_with_Concatenation_of_All_Words/main.cpp
@@ -6,10 +6,25 @@
 //  Copyright © 2015年 中央政治局常委. All rights reserved.
 //
 
+#include "solution.h"
 #include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+#include <set>
+#include <fstream>
+using namespace std;
 
+
+fstream handle("/Users/bingoboy/Downloads/cases.txt");
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Solution test;
+    vector<string> words;
+    string s, word;
+    handle >> s;
+    while (handle >> word) {
+        words.push_back(word);
+    }
+    test.findSubstring(s, words);
     return 0;
 }
